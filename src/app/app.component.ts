3103345ece5d3b5545c12cfb89036a4ee3a9f713
Lucio neves <luciocecilia20@gmail.com>
2024-05-19 16:29:32 -0400

    feat: funcionalidade de click no botão de entrar e visualizar senha.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/components/login/login.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
@@ -8,7 +9,13 @@ import { LoginComponent } from "./components/components/login/login.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent]
+    imports: [
+      RouterOutlet, 
+      LoginComponent, 
+      ReactiveFormsModule, 
+     
+    
+    ]
 })
 export class AppComponent {
   title = 'desafiocursos';
--- src/app/components/components/login/login.component.ts
+++ src/app/components/components/login/login.component.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { PasswordModule } from 'primeng/password' 
+import { FormBuilder, FormGroup, Validators,  } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, PasswordModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
-
-  value!: string
   
+  formulario!: FormGroup;
+  
+  isPasswordVisible: { [key: string]: boolean } = { senha: true, confirm_senha: false };
 
-  constructor() {}
+ constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      matricula: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if(this.formulario.valid) {
+      console.log('Formulario validoi:', this,this.formulario.value);      
+    } else{
+      console.log('Forlario invalido');      
+    }
+  }
+
+  onEntrarClick() {
+    console.log('Botão clicado');   
+  }
+
+  togglePasswordVisibility(controlName: string){
+    this.isPasswordVisible[controlName] = !this.isPasswordVisible[controlName];
+    
+
+    console.log('olho fechado');
     
   }
 
