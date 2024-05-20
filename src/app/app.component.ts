ce951da495978c3920713220428944201fae4443
Lucio neves <luciocecilia20@gmail.com>
2024-05-20 13:10:04 -0400

    feat: estrutuda html home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./components/components/login/login.component";
 import { ReactiveFormsModule } from '@angular/forms';
-import { SidebarModule } from 'primeng/sidebar';
-
-
+import { MenuModule } from 'primeng/menu';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
@@ -15,10 +14,9 @@ import { SidebarModule } from 'primeng/sidebar';
       RouterOutlet, 
       LoginComponent, 
       ReactiveFormsModule,
-      SidebarModule,
-      
-     
-    
+      MenuModule,
+      CommonModule,
+
     ]
 })
 export class AppComponent {
--- src/app/components/components/header/header.component.ts
+++ src/app/components/components/header/header.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AvatarModule} from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
-import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+
+
+
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [AvatarModule, AvatarGroupModule, SidebarModule, ButtonModule],
+  imports: [AvatarModule, AvatarGroupModule, ButtonModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'],
+ 
 })
-export class HeaderComponent {
-  sidebarVisible: boolean = false;
+export class HeaderComponent implements OnInit {
+ 
+
+  constructor() {}
+
+  
+
+ 
+
+    ngOnInit() {
+     
+    }
+
 }
